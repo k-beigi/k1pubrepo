cd97302009c469c66c5a0fb9679f6c3e4d520e8c
K1 Beigi <36334860+k-beigi@users.noreply.github.com>
2023-12-26 17:27:48 +0330

    Add files via upload

---
+++ NX-API/nx-api.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import pprint
+import urllib3
+
+class nxapi_json:
+    def __init__(self):
+        self.switchuser= input("Enter Username: ")
+        self.switchpassword= input("Enter Password: ")
+        self.url='https://172.30.8.123/ins'
+        self.myheaders={'content-type':'application/json'}
+        self.payload={
+          "ins_api": {
+            "version": "1.0",
+            "type": "cli_show",
+            "chunk": "0",
+            "sid": "sid",
+            "input": "sh ip int br vrf all",
+            "output_format": "json"
+          }
+        }
+
+    def run_nxapi(self):
+        urllib3.disable_warnings()
+        response = requests.post(self.url,data=json.dumps(self.payload), headers=self.myheaders,auth=(self.switchuser,self.switchpassword),verify=False).json()
+        #print(json.dumps(response, indent=4))
+        row_intf = response["ins_api"]["outputs"]["output"]["body"]["TABLE_intf"]["ROW_intf"]
+        #print(row_intf)
+        for i in range(len(row_intf)):
+            print(row_intf[i]['prefix'])
+
+obj1 = nxapi_json()
+obj1.run_nxapi()
